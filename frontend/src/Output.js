592b82525ffdaccf785c5cce8b9c873dd1eb1e24
Ayush <ayush4002gupta@gmail.com>
2023-06-05 13:39:32 +0530

    added copy button

--- frontend/src/Output.js
+++ frontend/src/Output.js
@@ -1,44 +1,57 @@
-import React from 'react'
+import React from "react";
 import { Table, Row, Col } from "react-bootstrap";
+import "./App.css";
 
 export default function Output(props) {
-    const { outputs = [], imageToPredict } = props;
+  const { outputs = [], imageToPredict } = props;
 
-
-    return (
-        <Row className="mt-3">
-            <Col>
-                <div className="mb-3 row justify-content-center align-items-flex-start">
-                    {imageToPredict ? <img src={imageToPredict} style={{
-                        width: "100%",
-                        maxWidth: "400px",
-                        height: "auto"
-                    }} alt="img-to-predict"
-                    /> : <div>
-                        Image to predict will be shown here
-                    </div>}
+  return (
+    <Row className="mt-3">
+      <Col>
+        <div className="mb-3 row justify-content-center align-items-flex-start">
+          {imageToPredict ? (
+            <img
+              src={imageToPredict}
+              style={{
+                width: "100%",
+                maxWidth: "400px",
+                height: "auto",
+              }}
+              alt="img-to-predict"
+            />
+          ) : (
+            <div>Image to predict will be shown here</div>
+          )}
+        </div>
+      </Col>
+      <Col lg={8} md={6} sm={12} xs={12}>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>
+                <div className="copy">
+                  <div>Caption</div>
+                  <button className="copybutton"
+                    onClick={() => {
+                      navigator.clipboard.writeText(outputs);
+                      document.getElementById("custom-tooltip").style.display = "inline";
+                      setTimeout(function () {
+                          document.getElementById("custom-tooltip").style.display = "none";
+                      }, 1000);
+                    }}
+                  >
+                    <img src={require("./Assets/copymain2.png")} alt="" style={{"width":"20px"}} />
+                  </button>
+                  <span id="custom-tooltip">copied!</span>
                 </div>
-            </Col>
-            <Col lg={8} md={6} sm={12} xs={12}>
-                <Table hover>
-                    <thead>
-                        <tr>
-                            <th>Caption    <button 
-                            onClick={() => {
-                            navigator.clipboard.writeText(outputs);
-                            // document.getElementById("custom-tooltip").style.display = "inline";
-                            // setTimeout(function () {
-                            //     document.getElementById("custom-tooltip").style.display = "none";
-                            // }, 1000);
-                        }}>copy</button></th>
-                            {/* <th>Label</th>
+              </th>
+              {/* <th>Label</th>
                             <th>Probability</th> */}
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {outputs}
-                        {/* { 
+            </tr>
+          </thead>
+          <tbody>
+            {outputs}
+            {/* { 
                             outputs.map((o, i) => {
                             return <tr key={i}>
                                 <td>{i + 1}</td>
@@ -46,9 +59,9 @@ export default function Output(props) {
                                 <td>{o.value}</td>
                             </tr>
                         })} */}
-                    </tbody>
-                </Table>
-            </Col>
-        </Row>
-    )
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
 }
