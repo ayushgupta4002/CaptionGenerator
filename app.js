bdd19c2953564f748b256cced9607fb0b9c91a43
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2023-06-07 22:23:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post("/chat",(req,res) => {
 
 
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5000';
 
 app.set('port', port);
 const server = http.createServer(app);
